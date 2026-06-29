e5c5b81f1408eff24a143a1004f9045736df498e
iplaynasty <amansharma1841112016@gmail.com>
2026-06-29 05:44:52 +0530

    Update and rename custom_Spacewar.mk to infinity_Spacewar.mk

--- infinity_Spacewar.mk
+++ infinity_Spacewar.mk
@@ -13,10 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/nothing/Spacewar/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := custom_Spacewar
+PRODUCT_NAME := infinity_Spacewar
 PRODUCT_DEVICE := Spacewar
 PRODUCT_BRAND := Nothing
 PRODUCT_MODEL := A063
@@ -33,24 +33,23 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Blur
 TARGET_ENABLE_BLUR := true
 
-# Boot Animation
-TARGET_BOOT_ANIMATION_RES := 1080
+#Boot Animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
 
 TARGET_CUSTOM_UDFPS := true
 
-WITH_GMS := true
+WITH_GAPPS := true
 
 WITH_BCR := true
 
-$(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
-$(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
+# Maintainer Name
+INFINITY_MAINTAINER := iplaynasty
 
-SURFACE_FLINGER_BOOST := true
-
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/pixel/certs/releasekey
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/infinity/certs/releasekey
 
 # 2. Bluetooth mainline module SEPolicy signed with your keys (CRITICAL)
 PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := $(dir $(PRODUCT_DEFAULT_DEV_CERTIFICATE))
 
 # 3. Recovery can verify OTA zips signed with your keys
-PRODUCT_EXTRA_RECOVERY_KEYS := vendor/pixel/keys/releasekey
+PRODUCT_EXTRA_RECOVERY_KEYS := vendor/lineage-priv/keys/releasekey
